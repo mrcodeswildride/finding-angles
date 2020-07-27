b369c8b9ea16c1f86da12f25baad5e8327ff7496
David Keller <mrcodeswildride@gmail.com>
2020-07-27 20:56:23 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# finding-angles
+
+Finding angles web app written in HTML, CSS, and JavaScript.
+
+Use this app at [angles.mrcodeswildride.com](https://angles.mrcodeswildride.com/).
+Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
---
+++ script.js
@@ -0,0 +1,23 @@
+let rotationParagraph = document.getElementById(`rotationParagraph`)
+let image = document.getElementById(`image`)
+
+document.addEventListener(`mousemove`, findAngle)
+
+function findAngle(event) {
+  let imageX = image.offsetLeft + (image.offsetWidth / 2)
+  let imageY = image.offsetTop + (image.offsetHeight / 2)
+
+  let opposite = -(event.offsetY - imageY)
+  let adjacent = event.offsetX - imageX
+  let radians = Math.atan2(opposite, adjacent)
+  let degrees = radians * (180 / Math.PI)
+
+  let imageRotation = -degrees + 90
+  let normalizedRotation = (imageRotation + 360) % 360
+
+  let rotation = Math.round(normalizedRotation)
+  let rotationUnits = rotation == 1 ? `degree` : `degrees`
+
+  rotationParagraph.innerHTML = `Rotation: ${rotation} ${rotationUnits}`
+  image.style.transform = `rotate(${rotation}deg)`
+}
